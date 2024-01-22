113e3e5f433c78daf1fac9313a0c10d00f27ad5d
zakha3v <imstan98@gmail.com>
2024-01-22 18:03:06 +0530

    CheckoutPageInteractive

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart = []
+export const cart = [{
+  productId:'1a',
+  quantity: 2,
+}, {
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 2
+}]
 
 export function addToCart(productId) {
   let matchingItem;

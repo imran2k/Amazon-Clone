56f3132360df86c9671e5f210d13eb513029f59a
zakha3v <imstan98@gmail.com>
2024-01-29 23:54:32 +0530

    path fix

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
-import { renderOrderSummary } from "scripts/checkout/orderSummary.js";
-import { renderPaymentSummary } from "scripts/checkout/paymentSummary.js";
-import { calculatePaymentSummaryItems, calculateCheckoutQuantity } from "data/cart.js";
+import { renderOrderSummary } from "../scripts/checkout/orderSummary.js";
+import { renderPaymentSummary } from "../scripts/checkout/paymentSummary.js";
+import { calculatePaymentSummaryItems, calculateCheckoutQuantity } from "../data/cart.js";
 
 renderOrderSummary();
 renderPaymentSummary();
--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,7 +1,7 @@
-import { cart } from "data/cart.js"
-import { getProduct } from "data/products.js";
-import { getDeliveryOption } from "data/deliveryOptions.js";
-import { formatCurrency } from "scripts/utils/money.js";
+import { cart } from "../../data/cart.js"
+import { getProduct } from "../../data/products.js";
+import { getDeliveryOption } from "../../data/deliveryOptions.js";
+import { formatCurrency } from "../../scripts/utils/money.js";
 
 
 

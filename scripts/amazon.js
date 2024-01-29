67fccd882ff323a336da30322cce7e852d4bd974
Imran Khan <70747314+imran2k@users.noreply.github.com>
2024-01-29 23:22:14 +0530

    change paths

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,6 @@
-import { cart, addToCart, displayAddedMessage, calculateCartQuantity } from "../data/cart.js";
-import { products } from "../data/products.js";
-import { formatCurrency } from "./utils/money.js";
+import { cart, addToCart, displayAddedMessage, calculateCartQuantity } from "data/cart.js";
+import { products } from "data/products.js";
+import { formatCurrency } from "scripts/utils/money.js";
 
 
 let productsHTML = '';
